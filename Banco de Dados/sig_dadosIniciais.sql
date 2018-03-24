97f9f8876a31cc5ed7ac09da6666851bb6081ca6
karlissonJR <karlissonj@gmail.com>
2018-03-24 11:53:44 -0300

    dados iniciais

---
+++ Banco de Dados/sig_dadosIniciais.sql
@@ -0,0 +1,15 @@
+insert into gerente_de_estoque values
+	("11111111100", "Gabriel", 5000),
+    ("22222222200", "Zé", 1500.49),
+    ("33333333300", "Maria", 2000),
+    ("44556677889", "Willames Almeida", 12578.64);
+    
+insert into estoque values
+	("001E", 50),
+    ("010E", 30);
+    
+insert into produto values
+	("0001PE", "Pen Drive 8GB", 50, "001E"),
+    ("0002PE", "Pipoca", 0.10, "010E"),
+    ("0003PE", "Miojo", 1.5, "010E"),
+    ("0004PE", "Cocada", 0.1, "010E");
